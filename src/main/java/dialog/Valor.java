763a7bb52b1a9066e7c7f9b2666938d71a96f093
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2018-05-20 17:36:56 +0200

    mend

---
+++ src/main/java/dialog/Valor.java
@@ -0,0 +1,5 @@
+package dialog;
+
+public class Valor {
+    private String valor;
+}
---
+++ src/main/java/dialog/ValorOriginal.java
@@ -0,0 +1,9 @@
+package dialog;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ValorOriginal {
+    @SerializedName("valor.original")
+    private String valorOriginal;
+    private String valor;
+}
